ffbf8e011f6bd338e59d750f73afccdff5a38a12
ramesh-monisha <144646444+ramesh-monisha@users.noreply.github.com>
2023-09-11 20:54:34 +0530

    Update streamlit_app.py
    fixed mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Healthy day')
-streamlit.header('breakfast iteams')
+streamlit.header('breakfast items')
 streamlit.text('soaked oats with milk')
 streamlit.text('hard-boiled free range egg')
 streamlit.text('smoothie')
