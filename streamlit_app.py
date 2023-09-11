608ad8b6b63817de40624bf960cd72a3db290cf2
ramesh-monisha <144646444+ramesh-monisha@users.noreply.github.com>
2023-09-11 20:53:19 +0530

    Update streamlit_app.py
    Menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Healthy day')
+streamlit.header('breakfast iteams')
+streamlit.text('soaked oats with milk')
+streamlit.text('hard-boiled free range egg')
+streamlit.text('smoothie')
